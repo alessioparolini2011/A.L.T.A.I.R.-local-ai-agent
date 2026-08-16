7e4a28ed5d5450837d116cabb06b7566d4b30331
Alessio <alepar.ap9@gmail.com>
2026-08-16 15:06:52 +0200

    Now the chat has an updating history

--- ai_connect.py
+++ ai_connect.py
@@ -10,16 +10,30 @@ import requests
 
 #putting in a variable the Ollama URL to speak with the model  --> DON'T CHANGE <--
 
-URL = "http://localhost:11434/api/generate"
+URL = "http://localhost:11434/api/chat"
+
+#creating the chat history
+
+history = [
+    {
+        "role" : "system",
+        "content" : "Sei un assistente AI dedicato alla conversazione. Sii conciso e colloquiale."
+    }
+]
 
 
-def request():
 
-    prompt = input("Fai una domanda all'IA: ")
+
+def request(prompt):
+
+    history.append({
+        "role" : "user", 
+        "content" : prompt
+    })
 
     payload = {
     "model" : "llama3.2",
-    "prompt" : prompt,
+    "messages" : history,
     "options": {
         "num_predict" : 400,
         "temperature" : 0.4
@@ -30,7 +44,16 @@ def request():
     #send the prompt to the AI model 
 
     req = requests.post(URL, json=payload)
-    response = req.json()["response"]
+    response = req.json()["message"]["content"]
+
+    #save the response in the history
+
+    history.append(
+    {
+        "role" : "assistant",
+        "content" : response
+    }
+    )
 
     return response
 
