a168117bd44e393d317967d09405875584006767
Alessio <alepar.ap9@gmail.com>
2026-08-16 18:09:58 +0200

    "Module for voice input"

---
+++ sttext.py
@@ -0,0 +1,52 @@
+import queue
+
+from vosk import Model, KaldiRecognizer
+
+import json 
+
+import sounddevice as sd
+
+#creating the fifo object
+
+q = queue.Queue()
+
+def callback(indata, frames, time, status):
+
+    #putting the audio pack in the list 
+
+    q.put(bytes(indata))
+
+    return frames, time, status
+
+
+def ascolta():
+
+    model = Model("model")
+
+    reco = KaldiRecognizer(model, 16000)
+
+    #
+
+    with sd.RawInputStream(
+        samplerate=16000,
+        blocksize=8000,
+        dtype="int16",
+        channels=1, 
+        callback=callback,
+    ):
+        while True: 
+            data = q.get()
+            if reco.AcceptWaveform(data):
+
+                voice = json.loads(reco.Result())
+
+                text = voice["text"]
+
+                if text:
+                    return text
+
+if __name__ == "__main__":
+    print("In ascolto... Parla pure!")
+    frase = ascolta()
+    print(f"Hai detto: {frase}")
+    
