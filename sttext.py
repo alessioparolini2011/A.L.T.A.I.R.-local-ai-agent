919b40a12a2f0e2c90d7b03d5b327491aab37b15
Alessio <alepar.ap9@gmail.com>
2026-08-29 22:56:42 +0200

    Now wait for the TTS to start, avoiding loops

--- sttext.py
+++ sttext.py
@@ -1,24 +1,35 @@
 import queue
+
 from vosk import Model, KaldiRecognizer
+
 import json 
+
 import sounddevice as sd
+
+from shared import switcher
+
 
 #creating the fifo object
 q = queue.Queue()
 
 #initializing the vosk model
 model = Model("model")
+
 reco = KaldiRecognizer(model, 16000)
 
 def callback(indata, frames, time, status):
+
     #putting the audio pack in the list 
+    
     if status:
         print(status)
+
     q.put(bytes(indata))
 
 def hear():
 
     #starts to get datas from microphone
+
     with sd.RawInputStream(
         samplerate=16000,
         blocksize=8000,
@@ -31,9 +42,14 @@ def hear():
         while not q.empty():
             q.get()
 
-        print("IA in ascolto...")
+
+        print("IA is ready to listen, please speak...")
         
         while True: 
+
+            if switcher.is_set(): #if the switcher is True, it means the TTS is running, so the STT must wait
+
+                continue
 
             #put every audio block in the FIFO object
             data = q.get()
