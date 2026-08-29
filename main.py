a151f9c050b03bc273120558c250185d0b4dfb9b
Alessio <alepar.ap9@gmail.com>
2026-08-29 22:51:13 +0200

    Now manage the double thread without interferences. Also fixed all bugs.

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-''''This is the main file. It groups together the functions by others files and call it. '''
+''''This is the main file. It groups together the functions by others files and call it. Also manage double threading. '''
 
-# import functions from ttspeech.py and ai_connect.py files
+#Import the functions and objects from the other files
 
 from ttspeech import speech
 
 from ai_connect import request
 
-from sttext import ascolta
+from sttext import hear
+
+import threading
+
+from shared import switcher
+
+#initialazing double thread
+
+speakT = threading.Thread(target=speech, args=(), daemon=True)
 
 
 def main():
 
+    speakT.start()
+
     while True:
 
-        frase = ascolta()
+        phrase = hear()
 
-        output = request(frase)
+        request(phrase)
 
-        speech()
-
+        switcher.wait()
 
 
 if __name__ == "__main__":
