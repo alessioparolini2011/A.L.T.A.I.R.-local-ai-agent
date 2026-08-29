8547921e35a2709429b6e9cca4a1f39c42f6434a
Alessio <alepar.ap9@gmail.com>
2026-08-29 22:58:29 +0200

    Now manage the FIFO list shared with ai_connect and the stop sign in the right mode

--- ttspeech.py
+++ ttspeech.py
@@ -2,22 +2,40 @@
 
 import pyttsx4
 
-from shared import r
+from shared import r, switcher
 
 #initialazing the tts engine and some others funcitons
 
-engine = pyttsx4.init()
 
-engine.setProperty("rate", 120)
-
-voice = r"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_IT-IT_ELSA_11.0"
-
-engine.setProperty("voice", voice )
 
 def speech():
 
-    tts= r.get()
+    #initialazing the tts engine and some others funcitons
 
-    engine.say(tts)
+    engine = pyttsx4.init()
 
-    engine.runAndWait()
+    engine.setProperty("rate", 120)
+
+    voice = r"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_IT-IT_ELSA_11.0"
+
+    engine.setProperty("voice", voice )
+
+    print("Secondary TTS is running...")
+
+    while True:
+
+        tts= r.get()
+
+        if tts == None:
+
+            switcher.clear() #if the TTS is finished, set the switcher to False to let the STT to run
+
+            continue
+
+        else: 
+
+            switcher.set() #if the TTS is running, set the switcher to True to avoid the STT to run and create a loop
+
+            engine.say(tts)
+
+            engine.runAndWait()
