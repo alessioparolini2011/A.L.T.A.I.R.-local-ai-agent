a44747474a9324f0f33df594859c404e2cf5f7b4
Alessio <alepar.ap9@gmail.com>
2026-08-29 22:55:42 +0200

    This file contains all the shared objects and variables

--- shared.py
+++ shared.py
@@ -4,5 +4,10 @@ Here you can find the global variables and objects shared in the entire project
 
 import queue 
 
+import threading
 
 r = queue.Queue()
+
+#creating a  threading switcher to get sure TTS and STT doesn't run at the same time, to avoid the AI model to hear its own voice and create a loop.
+
+switcher = threading.Event()
