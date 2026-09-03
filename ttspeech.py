0a52d3afd610080d88042c89065f906c03bfbea2
Alessio <alepar.ap9@gmail.com>
2026-09-03 22:26:26 +0200

    Now use a secondary thread to manage the library pyttsx4. Soon updates to optmize with PiperTTS.

--- ttspeech.py
+++ ttspeech.py
@@ -1,41 +1,56 @@
-#importing the librarie to create and play the response 
+'''
+This file is used to give a voice to the AI model. 
+It uses the pyttsx4 library to convert the text response from the AI model into speech.
+It runs in a separate thread, so can start talking while the AI response is still being generated.
+'''
+
+
+#importing the libraries to play the audio response from the AI model and to use asynchronous functions
 
 import pyttsx4
 
-from shared import r, switcher
+import asyncio
 
-#initialazing the tts engine and some others funcitons
+#asyncio FIFO list, blocker and switcher to avoid loops
+
+switcher = asyncio.Event()
+
+blocker = False
+
+res = asyncio.Queue() #creating a FIFO list that to get AI splitted response from ai_connect
 
 
+async def caller(): #to activate the TTS function in a separate thread
 
-def speech():
+    while True:
 
-    #initialazing the tts engine and some others funcitons
+        phrase =  await res.get()
+
+        if phrase is not None:
+
+            if phrase.strip():
+
+                await asyncio.to_thread(speak, phrase)
+
+        else: 
+
+            print("A.L.T.A.I.R. finished. Is your turn now. ")
+
+            switcher.set()
+
+        
+def speak(text): #to give a voice to the AI model
+
+    #initializing engine, voice and speed (rate)
 
     engine = pyttsx4.init()
 
-    engine.setProperty("rate", 120)
+    engine.setProperty("rate", 125) #set the speed of the voice
 
     voice = r"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_IT-IT_ELSA_11.0"
 
     engine.setProperty("voice", voice )
 
-    print("Secondary TTS is running...")
+    engine.say(text)
 
-    while True:
-
-        tts= r.get()
-
-        if tts == None:
-
-            switcher.clear() #if the TTS is finished, set the switcher to False to let the STT to run
-
-            continue
-
-        else: 
-
-            switcher.set() #if the TTS is running, set the switcher to True to avoid the STT to run and create a loop
-
-            engine.say(tts)
-
-            engine.runAndWait()
+    engine.runAndWait()
